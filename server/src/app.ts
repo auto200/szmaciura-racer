be98892a7e17667d6e27e203e343236f6a986d82
auto200 <michal.warac@gmail.com>
2020-11-04 22:13:32 +0100

    Initial rooms logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,10 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import socketio from "socket.io";
+import { nanoid } from "nanoid";
+import { SOCKET_EVENTS, ROOM_STATES } from "../../shared";
+import { Player, Room } from "../../shared/interfaces";
+import { ROOM_MAX_PLAYERS, CARS_COUNT } from "./constants";
 
 const app = express();
 
@@ -20,9 +24,62 @@ if (process.env.DEV_PORT) {
     console.log("listening", "no port specified");
   });
 }
+type RoomsObj = { [key: string]: Room };
 
+const publicRooms: RoomsObj = {};
+// const privateRooms: RoomsObj = {};
 const io = socketio(server);
 
-io.on("connection", (socket) => {
+io.of("/game").on("connection", (socket) => {
   console.log("new client connected");
+  socket.on("disconnect", (reason) => {
+    console.log("client disconnected", reason);
+  });
+
+  socket.on(SOCKET_EVENTS.JOIN_QUE, () => {
+    //Join existing room
+    const freeRoom = Object.values(publicRooms).find(
+      (room) => room.state === ROOM_STATES.WAITING
+    );
+    if (freeRoom && freeRoom.players.length < ROOM_MAX_PLAYERS) {
+      socket.join(freeRoom.id, (err) => {
+        if (err) return;
+        freeRoom.players.push(getNewPlayer(socket.id));
+        io.of("/game")
+          .to(freeRoom.id)
+          .emit(SOCKET_EVENTS.UPDATE_ROOM, freeRoom);
+      });
+    } else {
+      //Create new room
+      const roomId = nanoid(6);
+      console.log("room id", roomId);
+      const timeToExpire = 1000 * 60 * 3;
+      const newRoom: Room = {
+        id: roomId,
+        state: ROOM_STATES.WAITING,
+        players: [getNewPlayer(socket.id)],
+        expireTS: Date.now() + timeToExpire,
+      };
+      publicRooms[roomId] = newRoom;
+
+      setTimeout(() => {
+        delete publicRooms[roomId];
+        io.of("/game").to(roomId).emit(SOCKET_EVENTS.ROOM_EXPIRED);
+        console.log("Room:", roomId, "expired. Closing...");
+      }, timeToExpire);
+
+      socket.join(roomId, (err) => {
+        if (err) return;
+        socket.emit(SOCKET_EVENTS.UPDATE_ROOM, newRoom);
+      });
+    }
+  });
 });
+
+function getNewPlayer(id: string): Player {
+  return {
+    id,
+    progress: 0,
+    carIndex: (Math.random() * CARS_COUNT) | 0,
+  };
+}
