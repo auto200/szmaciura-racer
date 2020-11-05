929ff4f6f6370c00e32271459131f11c71e27eb3
auto200 <michal.warac@gmail.com>
2020-11-05 12:21:17 +0100

    Initial room logic

--- client/src/pages/online.tsx
+++ client/src/pages/online.tsx
@@ -1,27 +1,72 @@
-import React, { useEffect, ChangeEvent, useRef, useState } from "react";
-import { Link } from "gatsby";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+import { SOCKET_EVENTS } from "../../../shared/";
+import ProgressIndicator from "../components/ProgressIndicator";
+import { useCarsContext } from "../contexts/CarsContext";
+import { Room } from "../../../shared/interfaces";
+import { GameModeLink } from "../components/sharedStyled";
+
+const JoinRace = styled.div`
+  font-size: 2.3rem;
+  color: ${({ theme }) => theme.colors.golden};
+  border-bottom: 2px solid black;
+  border-radius: 10px;
+  padding: 5px;
+  cursor: pointer;
+`;
+const ProgressContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+enum STATES {
+  INITIAL,
+  IN_QUE,
+}
 
 const Online: React.FC = () => {
-  const [socket, setSocket] = useState<SocketIOClient.Socket>(() =>
-    io(process.env.SOCKET_URL!)
-  );
+  const [socket] = useState<SocketIOClient.Socket>(() => {
+    const socket = io(process.env.SOCKET_URL!);
+    socket.on(SOCKET_EVENTS.COUNTDOWN_START, () => {});
+    socket.on(SOCKET_EVENTS.UPDATE_ROOM, (room: Room) => setRoom(room));
+    socket.on(SOCKET_EVENTS.ROOM_EXPIRED, () => {});
+    return socket;
+  });
+  const { cars } = useCarsContext();
+  const [state, setState] = useState(STATES.INITIAL);
+  const [room, setRoom] = useState<Room>();
+
   useEffect(() => {
-    console.log(socket);
-    if (socket) {
-      socket.on("xd", (data: any) => {
-        console.log(data);
-      });
-    }
     return () => {
       socket.disconnect();
     };
   }, [socket]);
 
+  const joinQue = () => {
+    socket.emit(SOCKET_EVENTS.JOIN_QUE);
+    setState(STATES.IN_QUE);
+  };
+
   return (
     <Layout>
-      <Link to="/">Go back to offline</Link>
+      <GameModeLink to={"/"}>
+        <button>Powrót do offline</button>
+      </GameModeLink>
+      {state !== STATES.IN_QUE && (
+        <JoinRace onClick={joinQue}>
+          Weź udział w wyścigu o złote galoty
+        </JoinRace>
+      )}
+      {room && (
+        <ProgressContainer>
+          <ProgressIndicator
+            players={room.players}
+            highlightPlayer={socket.id}
+          />
+        </ProgressContainer>
+      )}
     </Layout>
   );
 };
