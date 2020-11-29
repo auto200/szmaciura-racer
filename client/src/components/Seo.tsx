586015eb2023657c506f83dd7e92b9c9dc7ea6bc
auto200 <michal.warac@gmail.com>
2020-11-29 20:12:43 +0100

    Fix title

--- client/src/components/Seo.tsx
+++ client/src/components/Seo.tsx
@@ -51,7 +51,7 @@ const SEO: React.FC<Props> = ({
         },
         {
           property: `og:title`,
-          content: title,
+          content: site.siteMetadata.title,
         },
         {
           property: `og:description`,
