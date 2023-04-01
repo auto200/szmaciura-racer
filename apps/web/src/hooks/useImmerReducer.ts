6c1959dca5d94416ed415158646e6c0f51ca9ccd
auto200 <michal.warac@gmail.com>
2023-04-01 18:30:23 +0200

    feat: update immer reducer

--- apps/web/src/hooks/useImmerReducer.ts
+++ apps/web/src/hooks/useImmerReducer.ts
@@ -1,19 +1,35 @@
-import produce, { Draft } from "immer";
-import { useReducer, useMemo, Dispatch } from "react";
+import produce, { Draft, nothing } from "immer";
+import { Dispatch, useMemo, useReducer } from "react";
 
 // source: https://github.com/immerjs/use-immer
-
-export type Reducer<S = any, A = any> = (
+export type ImmerReducer<S, A> = (
   draftState: Draft<S>,
   action: A
-) => void | S;
+) => void | (S extends undefined ? typeof nothing : S);
 
-export function useImmerReducer<S = any, A = any>(
-  reducer: Reducer<S, A>,
-  initialState: S,
-  initialAction?: (initial: any) => S
+export function useImmerReducer<S, A, I>(
+  reducer: ImmerReducer<S, A>,
+  initializerArg: S & I,
+  initializer: (arg: S & I) => S
 ): [S, Dispatch<A>];
-export function useImmerReducer(reducer, initialState, initialAction) {
+
+export function useImmerReducer<S, A, I>(
+  reducer: ImmerReducer<S, A>,
+  initializerArg: I,
+  initializer: (arg: I) => S
+): [S, Dispatch<A>];
+
+export function useImmerReducer<S, A>(
+  reducer: ImmerReducer<S, A>,
+  initialState: S,
+  initializer?: undefined
+): [S, Dispatch<A>];
+
+export function useImmerReducer<S, A, I>(
+  reducer: ImmerReducer<S, A>,
+  initializerArg: S & I,
+  initializer?: (arg: S & I) => S
+) {
   const cachedReducer = useMemo(() => produce(reducer), [reducer]);
-  return useReducer(cachedReducer, initialState as any, initialAction);
+  return useReducer(cachedReducer, initializerArg as any, initializer as any);
 }
