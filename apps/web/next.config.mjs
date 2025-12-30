3f709988a29e90e02e1e1e017ed9fd6ba3c1da4f
auto200 <michal.warac@gmail.com>
2025-12-30 06:19:06 +0100

    export web as static page

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true,
   },
